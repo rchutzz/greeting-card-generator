e03bf88ef0d6e8ad8dd280ae3e85c053d75d96bc
hirichardhai <hirichardhai@gmail.com>
2018-08-30 20:01:48 -0700

    created local vue component in particular scope

--- src/main.js
+++ src/main.js
@@ -1,21 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.component('text-input', {
-  template: '<textarea :placeholder="textValue" @click="changeText"></textarea>',
-  data: function() {
-    return {
-      textValue: 'Type here'
-    }
-  },
-  methods: {
-    changeText: function() {
-      this.textValue = 'Text changed'
-    }
-  }
-})
+var component1 = {
+  template: '<p>Component 1!!!</p>'
+}
+
+var component2 = {
+  template: '<p>Component 2</p>'
+}
 
 new Vue({
   el: '#app',
+  components: {
+    'component1': component1
+  }
   // render: h => h(App)
 })
