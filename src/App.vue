9ed59f7f7f747b6c78aa33f0ef9abdad94b01dbe
hirichardhai <hirichardhai@gmail.com>
2018-08-30 21:19:46 -0700

    added card folder, CardFront.vue, and imported/ rendered it inside App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-sm-12">
         <nav-header></nav-header>
+        <card-front></card-front>
       </div>
     </div>
   </div>
@@ -10,14 +11,23 @@
 
 <script>
 import Header from './components/Header.vue'
+import CardFront from './components/card/CardFront.vue'
 
 export default {
   components: {
-    navHeader: Header
+    navHeader: Header,
+    cardFront: CardFront
   }
 }
 </script>
 
 <style>
+  body {
+    font-family: Verdana, Geneva, sans-serif;
+    color: #333;
+  }
 
+  a {
+    cursor: pointer
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,10 @@
     <div>
         <h1 class="text-center">Creative Cards</h1>
         <ul class="nav justify-content-center">
-            <li class="nav-item">Front</li>
-            <li class="nav-item">Inside Left</li>
-            <li class="nav-item">Inside Right</li>
-            <li class="nav-item">Back</li>
+            <li class="nav-item"><a class="nav-link">Front</a></li>
+            <li class="nav-item"><a class="nav-link">Inside Left</a></li>
+            <li class="nav-item"><a class="nav-link">Inside Right</a></li>
+            <li class="nav-item"><a class="nav-link">Back</a></li>
         </ul>
     </div>
 </template>
@@ -18,4 +18,8 @@
     h1 {
         margin-top: 10px;
     }
+    li {
+        margin: 0px 5px;
+    }
+    
 </style>
