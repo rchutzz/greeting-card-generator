363bc9f638c41d532f2ee94014d5af02d9347b80
hirichardhai <hirichardhai@gmail.com>
2018-08-30 19:43:39 -0700

    removed default vue app data. added global component text-input for testing

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.component('text-input', {
+  template: '<textarea :placeholder="textValue" @click="changeText"></textarea>',
+  data: function() {
+    return {
+      textValue: 'Type here'
+    }
+  },
+  methods: {
+    changeText: function() {
+      this.textValue = 'Text changed'
+    }
+  }
+})
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  // render: h => h(App)
 })
