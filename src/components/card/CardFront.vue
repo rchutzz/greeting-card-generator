4fd5ef4fb99c9496e4c3d496ccc1a866b0a50e4d
hirichardhai <hirichardhai@gmail.com>
2018-08-31 01:29:27 -0700

    created TextOutput component and rendered three in the card-display div

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -4,18 +4,19 @@
             <cc-text-input @displayTextChanged="textBoxValue1 = $event"></cc-text-input>
             <cc-text-input @displayTextChanged="textBoxValue2 = $event"></cc-text-input>
             <cc-text-input @displayTextChanged="textBoxValue3 = $event"></cc-text-input>
-       {{ textBoxValue1 }}
-       {{ textBoxValue2 }}
-       {{ textBoxValue3 }}
         </div>
         <div class="col-sm-6 card card-display">
-            
+            <cc-text-output></cc-text-output>
+            <cc-text-output></cc-text-output>
+            <cc-text-output></cc-text-output>
         </div>
     </div>
 </template>
 
 <script>
 import TextInput from './TextInput.vue'
+import TextOutput from './TextOutput.vue'
+
 
 export default {
     data: function() {
@@ -26,7 +27,8 @@ export default {
         }
     },
     components: {
-        ccTextInput: TextInput
+        ccTextInput: TextInput,
+        ccTextOutput: TextOutput
     }
 }
 </script>
---
+++ src/components/card/TextOutput.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Sample text</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+
+</style>
