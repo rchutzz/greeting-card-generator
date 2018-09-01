9781886f239bb64d198abae36e68ae0eaaab7a98
Richard Hai <hirichardhai@gmail.com>
2018-08-31 23:12:55 -0700

    created and added Footer component into App. added home, about us, contact us li tags in footer

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,17 @@
         <keep-alive>
           <component v-bind:is="currentPage"></component>
         </keep-alive>
+        <cc-footer>
+          <p class="text-center">&copy; Creative Cards</p>
+          <nav>
+            <ul class="nav justify-content-center">
+              <li class="nav-item"><a class="nav-link">Home</a></li>
+              <li class="nav-item"><a class="nav-link">About Us</a></li>
+              <li class="nav-item"><a class="nav-link">Contact Us</a></li>
+
+            </ul>
+          </nav>
+        </cc-footer>
       </div>
     </div>
   </div>
@@ -17,7 +28,7 @@ import CardFront from './components/card/CardFront.vue'
 import CardInsideLeft from './components/card/CardInsideLeft.vue'
 import CardInsideRight from './components/card/CardInsideRight.vue'
 import CardBack from './components/card/CardBack.vue'
-
+import Footer from './components/Footer.vue'
 
 export default {
   data: function() {
@@ -30,7 +41,8 @@ export default {
     cardFront: CardFront,
     cardInsideLeft: CardInsideLeft,
     cardInsideRight: CardInsideRight,
-    cardBack: CardBack
+    cardBack: CardBack,
+    ccFooter: Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    <slot></slot>
+  </footer>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
