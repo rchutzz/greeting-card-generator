ebe438a71e14f8fed92677c83cc9f36af919264c
Richard Hai <hirichardhai@gmail.com>
2018-09-01 23:48:48 -0700

    added html structure, props validation, and css styling into ImageOutput

--- src/components/card/ImageOutput.vue
+++ src/components/card/ImageOutput.vue
@@ -1,13 +1,27 @@
 <template>
-
+  <div class="img-container">
+    <img id="outputImage"> {{ displayImage }}
+  </div>
 </template>
 
 <script>
   export default {
-    
+    props: {
+      displayImage: {
+        type: String
+      },
+      containerHeight: {
+        type: Number,
+        default: 200
+      }
+    }
   }
 </script>
 
 <style>
-
+  .img-container {
+    border: 1px dotted grey;
+    overflow: hidden;
+    margin: 5px 0;
+  }
 </style>
