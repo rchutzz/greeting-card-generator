758d6d13b2bf9cdb951f5ddb052072eb52bd27cc
Richard Hai <hirichardhai@gmail.com>
2018-09-01 21:46:04 -0700

    added img preview into img tag using FileReader.onload

--- src/components/card/ImageUpload.vue
+++ src/components/card/ImageUpload.vue
@@ -3,7 +3,7 @@
     <div class="col-sm-12">
       <h4>Upload Image:</h4>
       <div class="form-group">
-        <input type="file" class="form-control-file" id="fileUpload">
+        <input type="file" class="form-control-file" id="fileUpload" @change="uploadFile">
       </div>
       <br>
       <img id="image">
@@ -13,10 +13,33 @@
 </template>
 
 <script>
-  export default {
+import Firebase from 'firebase'
 
+  export default {
+    data: function() {
+      return {
+        file: ''
+      }
+    },
+    methods: {
+      uploadFile: function(event) {
+        this.file = event.target.files[0];
+        let storageRef = Firebase.storage().ref('user_uploads/' + this.file.name);
+        storageRef.put(this.file);
+
+        var reader = new FileReader();
+        reader.readAsDataURL(this.file);
+
+        reader.onload = function(e) {
+          document.getElementById('image').src = e.target.result;
+        }
+      }
+    }
   }
 </script>
 
 <style>
+  img {
+    max-width: 200px;
+  }
 </style>
