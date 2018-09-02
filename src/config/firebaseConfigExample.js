546fde10c9c259690998f413543c3c6a79c510ad
Richard Hai <hirichardhai@gmail.com>
2018-09-01 17:26:21 -0700

    added firebase api key files and added restriction in gitignore

---
+++ src/config/firebaseConfigExample.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "creative-cards-app-bb48a.firebaseapp.com",
+  databaseURL: "https://creative-cards-app-bb48a.firebaseio.com",
+  projectId: "creative-cards-app-bb48a",
+  storageBucket: "creative-cards-app-bb48a.appspot.com",
+  messagingSenderId: "114027640873"
+};
+firebase.initializeApp(config);
