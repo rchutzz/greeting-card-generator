a931dad17e5e566dad4b04f270a86466f2c4ba0f
hirichardhai <hirichardhai@gmail.com>
2018-09-03 22:31:15 -0700

    filled in InsideRight.vue with 3 text-inputs and 3 text-output compnents

--- src/components/card/CardInsideRight.vue
+++ src/components/card/CardInsideRight.vue
@@ -1,16 +1,35 @@
 <template>
     <div class="row">
         <div class="col-sm-6 card edit-area">
-            <p>CardInsideRight</p>
+            <cc-text-input @displayTextChanged="textBoxValue1 = $event"></cc-text-input>
+            <cc-text-input @displayTextChanged="textBoxValue2 = $event"></cc-text-input>
+            <cc-text-input @displayTextChanged="textBoxValue3 = $event"></cc-text-input>
         </div>
         <div class="col-sm-6 card card-display">
-            
+            <cc-text-output :displayText="textBoxValue1" :containerHeight="240"></cc-text-output>
+            <cc-text-output :displayText="textBoxValue2" :containerHeight="240"></cc-text-output>
+            <cc-text-output :displayText="textBoxValue3" :containerHeight="240"></cc-text-output>
         </div>
     </div>
 </template>
 
 <script>
+import TextInput from './TextInput.vue'
+import TextOutput from './TextOutput.vue'
 
+export default {
+    data: function() {
+        return {
+            textBoxValue1: '',
+            textBoxValue2: '',
+            textBoxValue3: ''
+        }
+    },
+    components: {
+        ccTextInput: TextInput,
+        ccTextOutput: TextOutput
+    }
+}
 </script>
 
 <style>
