e7140a7390d5ff62fc312ed2aa4e78e54f0b8afe
hirichardhai <hirichardhai@gmail.com>
2018-09-03 22:02:14 -0700

    added import line inside to easily copy and paste into new firebaseConfig.js when re-downloading app

--- src/config/firebaseConfigExample.js
+++ src/config/firebaseConfigExample.js
@@ -1,11 +1,12 @@
 import Firebase from 'firebase'
 
 var config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "creative-cards-app-bb48a.firebaseapp.com",
-  databaseURL: "https://creative-cards-app-bb48a.firebaseio.com",
-  projectId: "creative-cards-app-bb48a",
-  storageBucket: "creative-cards-app-bb48a.appspot.com",
-  messagingSenderId: "114027640873"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "creative-cards-app-bb48a.firebaseapp.com",
+    databaseURL: "https://creative-cards-app-bb48a.firebaseio.com",
+    projectId: "creative-cards-app-bb48a",
+    storageBucket: "creative-cards-app-bb48a.appspot.com",
+    messagingSenderId: "114027640873"
 };
+
 Firebase.initializeApp(config);
