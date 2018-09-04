ccae0575c9aeac838d857db6700c1d1c7c253d49
hirichardhai <hirichardhai@gmail.com>
2018-09-03 22:57:57 -0700

    added EventBus into main.js and added SectionCompleted.vue and imported into CardFront.vue

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -5,6 +5,7 @@
             <cc-image-upload @displayImageChanged="imageName = $event"></cc-image-upload><hr>
             <cc-text-input @displayTextChanged="textBoxValue2 = $event"></cc-text-input>
             <cc-text-input @displayTextChanged="textBoxValue3 = $event"></cc-text-input>
+            <cc-section-completed></cc-section-completed>
         </div>
         <div class="col-sm-6 card card-display">
             <cc-text-output :displayText="textBoxValue1" :containerHeight="150"></cc-text-output>
@@ -20,6 +21,7 @@ import TextInput from './TextInput.vue'
 import TextOutput from './TextOutput.vue'
 import ImageUpload from './ImageUpload.vue'
 import ImageOutput from './ImageOutput.vue'
+import SectionCompleted from './SectionCompleted.vue'
 
 export default {
     data: function() {
@@ -41,7 +43,8 @@ export default {
         ccTextInput: TextInput,
         ccTextOutput: TextOutput,
         ccImageUpload: ImageUpload,
-        ccImageOutput: ImageOutput
+        ccImageOutput: ImageOutput,
+        ccSectionCompleted: SectionCompleted
     }
 }
 </script>
---
+++ src/components/card/SectionCompleted.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="row">
+        <div class="form-check form-check-inline">
+            <div class="form-check-label">
+                <input type="checkbox" class="form-check-input"> Mark section Completed
+            </div>
+        </div>
+    </div>
+</template>
