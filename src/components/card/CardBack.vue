7ee6947165122d06ac9d096bae5cecee16142cc3
hirichardhai <hirichardhai@gmail.com>
2018-09-03 22:46:59 -0700

    added functioning CardBack.vue component

--- src/components/card/CardBack.vue
+++ src/components/card/CardBack.vue
@@ -1,27 +1,38 @@
 <template>
     <div class="row">
         <div class="col-sm-6 card edit-area">
-            <p>CardBack</p>
+            <cc-image-upload @displayImageChanged="imageName = $event"></cc-image-upload><hr>
         </div>
         <div class="col-sm-6 card card-display">
-            
+            <cc-image-output :displayImage="imageName" :containerHeight="400" :clearImageProp="clearImage"></cc-image-output>
         </div>
     </div>
 </template>
 
 <script>
+import ImageUpload from './ImageUpload.vue'
+import ImageOutput from './ImageOutput.vue'
 
+export default {
+    data: function() {
+        return {
+            imageName: ''
+        }
+    },
+    methods: {
+        clearImage: function() {
+            if (this.imageName !== '') {
+                this.imageName = 'beach.jpeg'
+            }
+        }
+    },
+    components: {
+        ccImageUpload: ImageUpload,
+        ccImageOutput: ImageOutput
+    }
+}
 </script>
 
 <style>
-    .edit-area {
-        background: #d2f9f9;
-        padding: 20px;
-        height: 800px;
-    }
 
-    .card-display {
-        height: 800px;
-        padding: 20px;
-    }
 </style>
