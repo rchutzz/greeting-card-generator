c62793357aa3535e42f3aa1a1665c28cccf6ab14
hirichardhai <hirichardhai@gmail.com>
2018-09-03 23:56:35 -0700

    edited webpack.config for loader issues

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
           'vue-style-loader',
           'css-loader'
         ],
-      },      {
+      },      
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
