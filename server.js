ebdaec9813aebf99e86f64b839eb623d88b82108
Richard Hai <hirichardhai@gmail.com>
2018-09-16 04:59:39 -0700

    serving root index.html instead of vue3 dist/index.html file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
